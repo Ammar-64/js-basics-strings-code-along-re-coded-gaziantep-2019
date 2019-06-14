a30ff03dfa2256e683a127cda5f48b1116f51867
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-06-14 11:50:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // Write your code in this file!
-const currentUser = 'Edsger Dijkstra';
+const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = ...;
-
-shortGreeting;
